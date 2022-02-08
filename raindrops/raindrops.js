c59b706e585b681b9e6babdf0eda3fbc828af868
Micah Sterling <msterling89@gmail.com>
2022-02-07 18:10:22 -0600

    add base solution

--- raindrops/raindrops.js
+++ raindrops/raindrops.js
@@ -3,6 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const convert = () => {
-  throw new Error('Remove this statement and implement this function');
+export const convert = (rain) => {
+  if (rain % 3 == 0) {
+    return "Pling"
+  } else if (rain % 5 == 0) {
+    return "Plang"
+  }else if (rain % 7 == 0) {
+    return "Plong"
+  }else {
+    return rain.toString()
+  }
 };
