261fef1f1d476b76117bd17614e957dea18b12ff
Micah Sterling <msterling89@gmail.com>
2022-02-16 19:05:40 -0600

    add step 1

--- isogram/isogram.js
+++ isogram/isogram.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const isIsogram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isIsogram = (string) => {
+  let check = new Set()
+  console.log(check)
+  for (let i = 0; i < string.length; i++) {
+    if (check == string[i]) {
+    check.add(string[i])
+    } else {
+      return false 
+    }
+  }
+  console.log(check)
 };
