6f5beb8db45da7a65e9880cb0911114ee0c372b8
Micah Sterling <msterling89@gmail.com>
2022-02-28 18:20:14 -0600

    fix case sensitive

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -5,17 +5,17 @@
 
 export const findAnagrams = (target,list) => {
   console.log(target,list)
-  let set = new Set(target) 
+  let set = new Set(target.toLowerCase()) 
   let output = []
   for (let i=0;i<list.length;i++) {
   let check = new Set()
     console.log("list 1",list[i])
-    if (list[i] === target) {
+    if (list[i].toLowerCase() === target.toLowerCase()) {
       check.add(false)
     }
     for (let j=0;j<list[i].length;j++) {
        // console.log("list 2",list[i][j])
-      if (set.has(list[i][j])) {
+      if (set.has(list[i][j].toLowerCase())) {
         // console.log("true")
         check.add(true)
       } else {
@@ -23,14 +23,13 @@ export const findAnagrams = (target,list) => {
         check.add(false)
       } 
     }
-  console.log("output",output)
-    if (check.has(false)) {
-      
-    } else {
+    console.log("output",output)
+    if (!check.has(false)) {
         output.push(list[i])
         console.log("output 1",output)
-    }
+    } 
     console.log("check",check)
   }
+  return output
 };
 
