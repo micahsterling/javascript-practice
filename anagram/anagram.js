d8abe5bd357f3259a40bd8a7c366d665f4f08588
Micah Sterling <msterling89@gmail.com>
2022-03-03 17:27:47 -0600

    clean up logs

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -4,20 +4,16 @@
 //
 
 export const findAnagrams = (target,list) => {
-  console.log(target,list)
-  console.log("target",target.length)
   let set = new Set(target.toLowerCase()) 
   let output = []
   for (let i=0;i<list.length;i++) {
   let objH = {}
   let check = new Set()
-    console.log("list 1",list[i])
     if (list[i].toLowerCase() === target.toLowerCase()) {
       check.add(false)
     }
       let count = 0
     for (let j=0;j<list[i].length;j++) {
-       // console.log("list 2",list[i][j])
       if (set.has(list[i][j].toLowerCase())) {
         check.add(true)
         count += 1
@@ -52,17 +48,10 @@ const isEqual = (objT,objH) => {
     }
   }
 }
- console.log("objects", isEqual(objT,objH))
-    
-    console.log("objT",objT)
-    console.log("objH",objH)
+  isEqual(objT,objH)
     if (!check.has(false) && count == target.length) {
         output.push(list[i])
-        console.log("output 1",output)
     } 
-    console.log("check",check)
   }
-  console.log("output",output)
   return output
 };
-
