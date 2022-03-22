00c1140d4c74f983118f5661d5aa678e571013cf
Micah Sterling <msterling89@gmail.com>
2022-03-22 17:17:57 -0500

    remove symbols/ add conditional

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -5,10 +5,20 @@
 
 export const countWords = () => {
   console.log(phrase);
-  let words = {}
-  let split = phrase.split(' ')
+  phrase = phrase.replace(/[&\/\\#+()$~%.":*?<>{}!@^]/g, '');
+   console.log("new",phrase);
+   let words = {}
+    let split = phrase.toLowerCase().split(/[\s,]+/)
   console.log(split)
   for (let i = 0; i < split.length; i++) {
-    console.log("split",split[i]);
-  };
+  console.log("split",split[i]);
+    if (words.hasOwnProperty(split[i])) {
+      words[split[i]] += 1
+    } else {
+      words[split[i]] = 1
+    }
+    
+  }
+  console.log(words)
+  return words
 };
